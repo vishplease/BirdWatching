149174f3020646b26b24fe7bd85e7333b6fc7f96
vishplease <vishalch@umich.edu>
2019-11-18 19:28:31 -0500

    Working App

--- app/src/main/java/com/example/birdwatching/Search.java
+++ app/src/main/java/com/example/birdwatching/Search.java
@@ -1,6 +1,7 @@
 package com.example.birdwatching;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,15 +9,37 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class Search extends AppCompatActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Search extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextZipSearch;
+    Button buttonZipSearch;
+    TextView textViewBirdNameResult, textViewPersonNameResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         this.setTitle("Search");
+
+        editTextZipSearch = findViewById(R.id.editTextZipSearch);
+        buttonZipSearch = findViewById(R.id.buttonZipSearch);
+        textViewBirdNameResult = findViewById(R.id.textViewBirdNameResult);
+        textViewPersonNameResult = findViewById(R.id.textViewPersonNameResult);
+
+        buttonZipSearch.setOnClickListener(this);
+
     }
 
     @Override
@@ -41,5 +64,59 @@ public class Search extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("birdsightings");
+
+        if (v == buttonZipSearch) {
+            //get text from editText
+
+            Integer newZipSearch = Integer.parseInt(editTextZipSearch.getText().toString());
+
+            myRef.orderByChild("zipCode").equalTo(newZipSearch).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    BirdSighting foundSighting = dataSnapshot.getValue(BirdSighting.class);
+
+                    String findName = foundSighting.personName;
+                    String findBird = foundSighting.birdName;
+
+                    textViewPersonNameResult.setText(findName);
+                    textViewBirdNameResult.setText(findBird);
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+
+        }
+
+
     }
 }
